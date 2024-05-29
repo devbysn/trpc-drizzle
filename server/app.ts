09379a8f4b4a4498107158895fe94429d336a381
System Administrator <root@MacBook-Pro.local>
2024-05-29 12:01:30 +0530

    Server starting restructured

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import type { CreateHTTPContextOptions } from '@trpc/server/adapters/standalone';
-import type { CreateWSSContextFnOptions } from '@trpc/server/adapters/ws';
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { appRouter } from './routers'
-import {applyWSSHandler} from '@trpc/server/adapters/ws'
-import ws from 'ws'
 
 const app = express()
 app.use(cors({origin: 'http://localhost:5173'}))
@@ -14,14 +10,10 @@ app.use("/trpc", trpcExpress.createExpressMiddleware({
 }))
 
 // Function to start the server
-export const server = app.listen(3000, () => {
+export const startServer = (()=>{
+    app.listen(3000, () => {
         console.log("Listening on server: 3000");
     });
-
-    applyWSSHandler({
-        wss: new ws.Server({ server }), // * <- pass the server
-        router: appRouter, // * <- pass the router
-        // createContext, // * <- pass the context
-      })
+})
       
 export type AppRouter = typeof appRouter
--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,10 @@
-import {db} from './drizzle/db'
-import { ProductTable, UserTable } from './drizzle/schema'
 import 'dotenv/config'
-import { server} from './app'
+import { startServer} from './app'
 import insertProducts from './drizzle/dummy_data'
 
 async function main() {
     // await insertProducts();
-    server
+    startServer()
 }
 
 main()
